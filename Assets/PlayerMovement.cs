96c1cc23f85eaa4b44d73764e36050c6fa6a1ab6
xgan68 <alex.x.gan@gmail.com>
2017-09-05 11:54:45 -0700

    Click to Move

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+			switch (cameraRaycaster.layerHit) {
+			case Layer.Walkable:
+				currentClickTarget = cameraRaycaster.hit.point;
+				break;
+
+			case Layer.Enemy:
+				print ("Not moving to enemy");
+				break;
+			default:
+				print ("Shouldn't be here");	
+				return;
+		}
+              // So not set in default case
+
+        }
+		m_Character.Move(currentClickTarget - transform.position, false, false);
+    }
+}
+
