5f137dbde860c0a9efd6416cf4d928f42dde4da9
xgan68 <alex.x.gan@gmail.com>
2017-09-06 15:54:40 -0700

    Added Cursor Affordance

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CameraRaycaster))]
+public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D targetCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+
+	[SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+	CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (cameraRaycaster.layerHit) {
+			case Layer.Walkable:
+				Cursor.SetCursor (walkCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.RaycastEndStop:
+				Cursor.SetCursor (unknownCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.Enemy:
+				Cursor.SetCursor (targetCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			default:
+				Debug.LogError ("Don't know which cursor to show");
+				return;
+		}
+
+	}
+}
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -20,32 +20,35 @@ public class PlayerMovement : MonoBehaviour
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
-        {
-            print("Cursor raycast hit " + cameraRaycaster.hit.collider.gameObject.name.ToString());
+        ProcessIndirectMovement ();
+
+    }
+
+	void ProcessIndirectMovement ()
+	{
+		if (Input.GetMouseButton (0)) {
+			//print("Cursor raycast hit " + cameraRaycaster.hit.collider.gameObject.name.ToString());
 			switch (cameraRaycaster.layerHit) {
 			case Layer.Walkable:
 				currentClickTarget = cameraRaycaster.hit.point;
 				break;
-
 			case Layer.Enemy:
 				print ("Not moving to enemy");
 				break;
 			default:
-				print ("Shouldn't be here");	
+				print ("Shouldn't be here");
 				return;
+			}
+			// So not set in default case
 		}
-              // So not set in default case
-
-        }
 		var playerToClickPoint = currentClickTarget - transform.position;
-		print (currentClickTarget);
+		//print (currentClickTarget);
 		if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
 			m_Character.Move (playerToClickPoint, false, false);
-		} else {
-			m_Character.Move(Vector3.zero, false, false);
 		}
-
-    }
+		else {
+			m_Character.Move (Vector3.zero, false, false);
+		}
+	}
 }
 
